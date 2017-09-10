3b811a9356fcb10a5c52c69be0e1805b59e76064
Jaekyoung Kim <rlakim5521@naver.com>
2017-09-10 04:21:18 +0000

    Change mysql host ip from localhost to 127.0.0.1

--- mysql_supporter.py
+++ mysql_supporter.py
@@ -4,5 +4,5 @@ from sqlalchemy import create_engine, exc, types
 user = os.getenv('C9_USER')
 password = ''
 database = 'findb'
-local_engine = create_engine('mysql+mysqlconnector://{}:{}@localhost/{}'.format(user, password, database),
+local_engine = create_engine('mysql+mysqlconnector://{}:{}@127.0.0.1/{}'.format(user, password, database),
                              encoding='utf-8')
